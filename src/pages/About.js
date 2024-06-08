2c4eb729d064498876bad4a9558dea4bccb1b256
ichow3 <ichow@cpp.edu>
2024-06-08 17:04:33 -0500

    hello

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react';
-  
+import React, { Component } from "react";
+import "./About.css";
+import pfp from "../assets/pfp.jpeg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img
+                className="profile_image"
+                src = {pfp}
+                alt="Profile Pic"
+              ></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Ian Chow</div>
+              <div className="brief_description">
+                Hi, My name is Ian Chow and I am a rising junior at Cal Poly Pomona studying computer science. I love to go to the gym and play basketball
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
